022d220307d155a08761503ac397682ca6e30baf
schrobingus <brent.monning.jr@gmail.com>
2024-12-05 14:39:39 -0800

    add completion and link to luasnip

--- config/default.nix
+++ config/default.nix
@@ -127,6 +127,46 @@ in
         };
       };
 
+      cmp = {
+        enable = true;
+        settings = {
+          sources = [
+            { name = "nvim_lsp"; }
+            { name = "path"; }
+            { name = "buffer"; }
+            { name = "luasnip"; }
+          ];
+          mapping = {
+            "<CR>" = ''
+              cmp.mapping.confirm({
+                select = false,
+                behavior = cmp.ConfirmBehavior.Replace
+              })
+            '';
+            "<Tab>" = ''
+              cmp.mapping(cmp.mapping.select_next_item({
+                behavior = cmp.SelectBehavior.Select
+              }), {"i", "s"})
+            '';
+            "<S-Tab>" = ''
+              cmp.mapping(cmp.mapping.select_prev_item({
+                behavior = cmp.SelectBehavior.Select
+              }), {"i", "s"})
+            '';
+
+            "<C-Space>" = "cmp.mapping.complete()";
+            "<C-e>" = "cmp.mapping.close()";
+            "<C-d>" = "cmp.mapping.scroll_docs(-4)";
+            "<C-f>" = "cmp.mapping.scroll_docs(4)";
+          };
+          snippet.expand = ''
+            function(args) 
+              require('luasnip').lsp_expand(args.body) 
+            end
+          '';
+        };
+      };
+
       # Configure plugins for LaTeX.
       vimtex = {
         enable = true;
